nickconway/caelestia

Add screenshot directory config option

utils/Paths.qml
@@ -19,6 +19,7 @@ Singleton {
     readonly property string imagecache: `${cache}/imagecache`
     readonly property string notifimagecache: `${imagecache}/notifs`
     readonly property string wallsdir: Quickshell.env("CAELESTIA_WALLPAPERS_DIR") || absolutePath(Config.paths.wallpaperDir)
+    readonly property string screenshotdir: Quickshell.env("CAELESTIA_SCREENSHOTS_DIR") || absolutePath(Config.paths.screenshotDir)
     readonly property string recsdir: Quickshell.env("CAELESTIA_RECORDINGS_DIR") || `${videos}/Recordings`
     readonly property string libdir: Quickshell.env("CAELESTIA_LIB_DIR") || "/usr/lib/caelestia"
 

osmargm1202/shell

utils/Paths.qml
@@ -21,6 +21,7 @@ Singleton {
     readonly property string notifimagecache: `${imagecache}/notifs`
     readonly property string wallsdir: Quickshell.env("CAELESTIA_WALLPAPERS_DIR") || absolutePath(GlobalConfig.paths.wallpaperDir)
     readonly property string videowallsdir: Quickshell.env("CAELESTIA_VIDEO_WALLPAPERS_DIR") || `${pictures}/Videos`
+    readonly property string screenshotdir: Quickshell.env("CAELESTIA_SCREENSHOTS_DIR") || absolutePath(GlobalConfig.paths.screenshotDir)
     readonly property string recsdir: Quickshell.env("CAELESTIA_RECORDINGS_DIR") || `${videos}/Recordings`
     readonly property string libdir: Quickshell.env("CAELESTIA_LIB_DIR") || "/usr/lib/caelestia"
 